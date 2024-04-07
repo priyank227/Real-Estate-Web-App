33ce3ba2f4567ad127dc08530dfe51f6e6dff50b
priyank viradiya <priyankviradiya227@gmail.com>
2024-04-07 17:40:14 +0530

    connect with mongodb

--- api/index.js
+++ api/index.js
@@ -1,5 +1,18 @@
-import express from `express`;
-const App = express();
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+// Connect to MongoDB Atlas
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log("Connected to MongoDB");
+    }
+).catch((err) => {
+    console.log(err);
+});
+
+const app = express();
+
 app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+  console.log("Server is running on port 3000");
 });
