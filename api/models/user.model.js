a6b3497c8d5230334e35d0c68b02c5a621939215
priyank viradiya <priyankviradiya227@gmail.com>
2024-04-08 23:56:15 +0530

    fetch profile photo from google and create profile page(frontend)

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -18,7 +18,7 @@ const userSchema = new mongoose.Schema(
     },
     avatar:{
       type:String,
-      default:"https://i.pinimg.com/564x/a8/57/00/a85700f3c614f6313750b9d8196c08f5.jpg"
+      default:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
     }
   },
   { timestamps: true }
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,26 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Home from "./pages/Home"
-import SignIn from "./pages/SignIn"
-import About from "./pages/About"
-import Profile from "./pages/Profile"
-import Header from "./components/Header"
-import SignUp from "./pages/SignUp"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Header from "./components/Header";
+import SignUp from "./pages/SignUp";
+import PrivateRoute from "./components/PrivateRoute";
 
 export default function App() {
   //to install browserroute library: npm i react-router-dom
-  return <BrowserRouter>
-  <Header/>
-  <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/sign-in" element={<SignIn />} />
-    <Route path="/sign-up" element={<SignUp />} />
-    <Route path="/about" element={<About />} />
-    <Route path="/profile" element={<Profile />} />
-  </Routes>
-  </BrowserRouter>
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
