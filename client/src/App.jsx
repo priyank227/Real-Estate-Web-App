d194d60449dcccb715ad8bf15faa9166dcdc18fa
priyank viradiya <priyankviradiya227@gmail.com>
2024-04-23 07:39:01 +0530

    update edit listing functionality.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import Header from "./components/Header";
 import SignUp from "./pages/SignUp";
 import PrivateRoute from "./components/PrivateRoute";
 import CreateListing from "./pages/CreateListing";
+import UpdateListing from "./pages/UpdateListing";
 
 export default function App() {
   //to install browserroute library: npm i react-router-dom
@@ -21,6 +22,8 @@ export default function App() {
         <Route element={<PrivateRoute />}>
           <Route path="/profile" element={<Profile />} />
           <Route path="/create-listing" element={<CreateListing />} />
+          <Route path="/update-listing/:listingId" element={<UpdateListing />} />
+
 
         </Route>
       </Routes>
