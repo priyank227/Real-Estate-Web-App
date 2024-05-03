a68449e6f861d1bcdbb81d25fe2b8f8338e2d220
priyank viradiya <priyankviradiya227@gmail.com>
2024-05-04 00:20:33 +0530

    update index.js

--- api/index.js
+++ api/index.js
@@ -1,28 +1,30 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import userRoute from './routes/user.route.js';
-import authRoute from './routes/auth.route.js';
-import listingRoute from './routes/listing.route.js';
-import cookieParser from 'cookie-parser';
-import path from 'path';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoute from "./routes/user.route.js";
+import authRoute from "./routes/auth.route.js";
+import listingRoute from "./routes/listing.route.js";
+import cookieParser from "cookie-parser";
+import path from "path";
 
 dotenv.config();
 
 // Connect to MongoDB Atlas
-mongoose.connect(process.env.MONGO).then(() => {
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
     console.log("Connected to MongoDB");
-    }
-).catch((err) => {
+  })
+  .catch((err) => {
     console.log(err);
-});
+  });
 
   const __dirname = path.resolve();
+
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
@@ -32,15 +34,15 @@ app.use("/api/user", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/listing", listingRoute);
 
-app.use(express.static(path.json(__dirname,'/client/dist')))
+app.use(express.static(path.join(__dirname, "/client/dist")));
 
-app.get('*',(req,res)=>{
-  res.sendFile(path.json(__dirname,'/client','dist','index.html'));
-})
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message || 'Internal Server Error';
+  const message = err.message || "Internal Server Error";
   return res.status(statusCode).json({
     success: false,
     statusCode,
